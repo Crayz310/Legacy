04fd243465af8fecf2ddf86c19a93b588326de4e
xdesai96 <xdesai@proton.me>
2025-11-23 16:35:46 +0100

    updated: aio_custom

--- legacy/aio_custom.py
+++ legacy/aio_custom.py
@@ -4,114 +4,132 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-from typing import Optional, Union, Any
-
-from aiogram import Bot
-from aiogram.client.session.base import BaseSession
-from aiogram.client.default import DefaultBotProperties
-
-from aiogram.types import InputFile, BufferedInputFile, FSInputFile, URLInputFile, InputFileUnion, Message
-
-from urllib.parse import urlparse
 import os
 from io import BytesIO
+from typing import Any, Optional, Union
+from urllib.parse import urlparse
+
+from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.base import BaseSession
+from aiogram.types import (BufferedInputFile, FSInputFile, InputFile,
+                           InputFileUnion, Message, URLInputFile)
+
 
 def _is_url_valid(url: str) -> bool:
-  """
-  Checks whether the string passed is a syntactically correct URL
+    """
+    Checks whether the string passed is a syntactically correct URL
 
-  Uses the built-in urlparse function to attempt to parse the string. Any error during parsing results in False being returned
+    Uses the built-in urlparse function to attempt to parse the string. Any error during parsing results in False being returned
 
-  :param url: String to be checked
-  :type url: str
-  :returns: True if the string is a valid URL, otherwise False
-  :rtype: bool
-  """
-  try:
-    urlparse(url)
+    :param url: String to be checked
+    :type url: str
+    :returns: True if the string is a valid URL, otherwise False
+    :rtype: bool
+    """
+    try:
+        urlparse(url)
 
-    return True
-  except Exception:
-    return False
+        return True
+    except Exception:
+        return False
+
+
+def _validate_file_type(document, filename):
+    if isinstance(document, bytes):
+        document = BufferedInputFile(document, filename=filename)
+    elif isinstance(document, BytesIO):
+        document = BufferedInputFile(
+            document.getvalue(),
+            filename=(
+                getattr(document, "name") if hasattr(document, "name") else filename
+            ),
+        )
+    elif isinstance(document, str):
+        if os.path.exists(document):
+            document = FSInputFile(document, filename=filename)
+        elif _is_url_valid(document):
+            document = URLInputFile(document, filename=filename)
+        else:
+            document = InputFileUnion(document)
+    return document
+
 
 class CustomBot(Bot):
-  """
-  Hybrid wrapper over `aiogram.Bot` for correct operation of modules that use aiogram 2.25
+    """
+    Hybrid wrapper over `aiogram.Bot` for correct operation of modules that use aiogram 2.25
 
-  Extends methods for automatic processing of various types of input data
-  """
+    Extends methods for automatic processing of various types of input data
+    """
 
-  def __init__(self, token: str, session: Optional[BaseSession] = None, default: Optional[DefaultBotProperties] = None, **kwargs: Any):
-    super().__init__(token=token, session=session, default=default, **kwargs)
-  
-  async def send_document(self, chat_id: Union[int, str], document: Union[InputFile, str], thumb: Optional[Union[InputFile, str]] = None, filename: str = None, **kwargs: Any) -> Message:
-    if isinstance(document, bytes):
-      document = BufferedInputFile(document, filename=filename)
-    elif isinstance(document, BytesIO):
-      document = BufferedInputFile(document.getvalue(), filename=(getattr(document, "name") if hasattr(document, "name") else filename))
-    elif isinstance(document, str):
-      if os.path.exists(document):
-        document = FSInputFile(document, filename=filename)
-      elif _is_url_valid(document):
-        document = URLInputFile(document, filename=filename)
-      else:
-        document = InputFileUnion(document)
+    def __init__(
+        self,
+        token: str,
+        session: Optional[BaseSession] = None,
+        default: Optional[DefaultBotProperties] = None,
+        **kwargs: Any
+    ):
+        super().__init__(token=token, session=session, default=default, **kwargs)
 
-    
-    params = {
-      "chat_id": chat_id,
-      "document": document,
-      "thumbnail": thumb
-    }
+    async def send_document(
+        self,
+        chat_id: Union[int, str],
+        document: Union[InputFile, str],
+        thumb: Optional[Union[InputFile, str]] = None,
+        filename: Optional[str] = None,
+        **kwargs: Any
+    ) -> Message:
+        document = _validate_file_type(document, filename)
 
-    params.update(kwargs)
+        params = {"chat_id": chat_id, "document": document, "thumbnail": thumb}
 
-    return await super().send_document(**{k: v for k, v in params.items()})
-  
-  async def send_photo(self, chat_id: Union[int, str], photo: Union[InputFile, str], **kwargs: Any) -> Message:
-    if isinstance(photo, bytes):
-      photo = BufferedInputFile(photo, filename=None)
-    elif isinstance(photo, BytesIO):
-      photo = BufferedInputFile(photo.getvalue(), filename=(getattr(photo, "name") if hasattr(photo, "name") else None))
-    elif isinstance(photo, str):
-      if os.path.exists(photo):
-        photo = FSInputFile(photo)
-      elif _is_url_valid(photo):
-        photo = URLInputFile(photo)
-      else:
-        photo = InputFileUnion(photo)
+        params.update(kwargs)
 
-    
-    params = {
-      "chat_id": chat_id,
-      "photo": photo
-    }
+        return await super().send_document(**{k: v for k, v in params.items()})
 
-    params.update(kwargs)
+    async def send_photo(
+        self,
+        chat_id: Union[int, str],
+        photo: Union[InputFile, str],
+        filename: Optional[str] = None,
+        **kwargs: Any
+    ) -> Message:
+        photo = _validate_file_type(photo, filename)
 
-    return await super().send_photo(**{k: v for k, v in params.items()})
-  
-  async def send_video(self, chat_id: Union[int, str], video: Union[InputFile, str], thumb: Optional[Union[InputFile, str]] = None,  **kwargs: Any) -> Message:
-    if isinstance(video, bytes):
-      video = BufferedInputFile(video, filename=None)
-    elif isinstance(video, BytesIO):
-      video = BufferedInputFile(video.getvalue(), filename=(getattr(video, "name") if hasattr(video, "name") else None))
-    elif isinstance(video, str):
-      if os.path.exists(video):
-        video = FSInputFile(video)
-      elif _is_url_valid(video):
-        video = URLInputFile(video)
-      else:
-        video = InputFileUnion(video)
+        params = {"chat_id": chat_id, "photo": photo}
 
-    
-    params = {
-      "chat_id": chat_id,
-      "video": video,
-      "thumbnail": thumb
-    }
+        params.update(kwargs)
 
-    params.update(kwargs)
+        return await super().send_photo(**{k: v for k, v in params.items()})
 
-    return await super().send_video(**{k: v for k, v in params.items()})
-  
+    async def send_video(
+        self,
+        chat_id: Union[int, str],
+        video: Union[InputFile, str],
+        thumb: Optional[Union[InputFile, str]] = None,
+        filename: Optional[str] = None,
+        **kwargs: Any
+    ) -> Message:
+        video = _validate_file_type(video, filename)
+
+        params = {"chat_id": chat_id, "video": video, "thumbnail": thumb}
+
+        params.update(kwargs)
+
+        return await super().send_video(**{k: v for k, v in params.items()})
+
+    async def set_chat_photo(
+        self,
+        chat_id: Union[int, str],
+        photo: Union[InputFile, str],
+        thumb: Optional[Union[InputFile, str]] = None,
+        filename: Optional[str] = None,
+        **kwargs: Any
+    ) -> bool:
+        photo = _validate_file_type(photo, filename)
+
+        params = {"chat_id": chat_id, "photo": photo}
+
+        params.update(kwargs)
+
+        return await super().set_chat_photo(**{k: v for k, v in params.items()})
