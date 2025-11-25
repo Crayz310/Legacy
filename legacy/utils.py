5f00d5ed5f717a28e1e4767623f394ea827bde8b
xdesai96 <xdesai@proton.me>
2025-11-25 15:35:49 +0100

    update #6: utils.answer (experimental)

--- legacy/utils.py
+++ legacy/utils.py
@@ -562,8 +562,8 @@ async def answer(
             result = await message.edit(
                 text,
                 parse_mode=lambda t: (t, entities),
-                media=file if not isinstance(file, TypeInputMedia) else None,
-                file=file if isinstance(file, TypeInputMedia) else None,
+                media=file if not isinstance(file, str) else None,
+                file=file if isinstance(file, str) else None,
                 **kwargs,
             )
         else:
